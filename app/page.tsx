d710e6471f71f8c3ba10e8d80092a8839c378fe9
Andreas Nordahl <andnorda@gmail.com>
2024-12-06 12:37:48 +0100

    fetch user from teamkatalogen and simple table

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,75 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Table } from "@navikt/ds-react";
+import {
+  TableBody,
+  TableDataCell,
+  TableHeader,
+  TableHeaderCell,
+  TableRow,
+} from "@navikt/ds-react/Table";
+import projects from "../data/projects.json";
+import users from "../data/users.json";
+
+const format = (date: Date) => {
+  const y = date.getFullYear();
+  const m = (date.getMonth() + 1).toString().padStart(2, "0");
+  const d = date.getDate().toString().padStart(2, "0");
+  return `${d}.${m}.${y}`;
+};
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <>
+      <h1>Users</h1>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderCell scope="col">displayName</TableHeaderCell>
+            <TableHeaderCell scope="col">navIdent</TableHeaderCell>
+            <TableHeaderCell scope="col">endDate</TableHeaderCell>
+            <TableHeaderCell scope="col">createdAt</TableHeaderCell>
+            <TableHeaderCell scope="col">updatedAt</TableHeaderCell>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {users.map(
+            (
+              { profile: { id, displayName, createdAt, updatedAt }, resource },
+              i
+            ) => {
+              return (
+                <TableRow key={id}>
+                  <TableHeaderCell scope="row">{displayName}</TableHeaderCell>
+                  <TableDataCell>{resource?.navIdent}</TableDataCell>
+                  <TableDataCell>{resource?.endDate}</TableDataCell>
+                  <TableDataCell>{format(new Date(createdAt))}</TableDataCell>
+                  <TableDataCell>{format(new Date(updatedAt))}</TableDataCell>
+                </TableRow>
+              );
+            }
+          )}
+        </TableBody>
+      </Table>
+      <h1>Projects</h1>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderCell scope="col">displayName</TableHeaderCell>
+            <TableHeaderCell scope="col">updatedAt</TableHeaderCell>
+            <TableHeaderCell scope="col">members</TableHeaderCell>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {projects.map(({ id, displayName, updatedAt, members }, i) => {
+            return (
+              <TableRow key={id}>
+                <TableHeaderCell scope="row">{displayName}</TableHeaderCell>
+                <TableDataCell>{format(new Date(updatedAt))}</TableDataCell>
+                <TableDataCell>{members.length}</TableDataCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </>
   );
 }
--- fetch.js
+++ fetch.js
@@ -3,6 +3,8 @@ const { writeFileSync, mkdirSync } = require("fs");
 const cookie = process.env.SANITY_SESSION;
 const orgId = "ojSsHMQGf";
 
+const teamkatalogenCookie = process.env.TEAMKATALOGEN_COOKIE;
+
 const f = (url) =>
   fetch(`https://api.sanity.io${url}`, { headers: { cookie } }).then((res) =>
     res.json()
@@ -28,6 +30,30 @@ Promise.all([
     .then(write("projects.json")),
   f(`/vX/access/organization/${orgId}/users?limit=500`)
     .then(({ data }) => data)
+    .then((users) =>
+      Promise.all(
+        users.map((user) =>
+          fetch(
+            `https://teamkatalog-api.intern.nav.no/resource/search/${user.profile.displayName}`,
+            { headers: { cookie: encodeURI(teamkatalogenCookie) } }
+          )
+            .then((res) => res.json())
+            .then((json) => json.content[0].navIdent)
+            .then((navIdent) =>
+              fetch(
+                `https://teamkatalog-api.intern.nav.no/resource/${navIdent}`,
+                { headers: { cookie: encodeURI(teamkatalogenCookie) } }
+              )
+            )
+            .then((res) => res.json())
+            .then((resource) => ({ ...user, resource }))
+            .catch((e) => {
+              console.error(user, e);
+              return user;
+            })
+        )
+      )
+    )
     .then(write("users.json")),
 ])
   .then(() => console.log("done"))
